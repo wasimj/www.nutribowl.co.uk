78fd0a75ddd5b637631840b1a742d25e48c762af
Wasim <w@juned.net>
2024-04-15 16:36:18 +0100

    intial

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+const version = 2 + 1
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="hello">
+    Welcome to Nutribowl
   </div>
 </template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
