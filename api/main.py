57b126f4f61645d5b4b9830f76814762bf0d033a
Wasim <w@juned.net>
2024-05-13 18:29:04 +0100

    add recipes api

--- api/main.py
+++ api/main.py
@@ -1,9 +1,25 @@
 # main.py
 
+from pydantic import BaseModel
 from fastapi import FastAPI
+from typing import List
 
 app = FastAPI()
 
-@app.get("/")
+class Recipe(BaseModel):
+    id: int
+    name: str
+
+@app.get("/recipes")
+
+def get_recipes():
+    recipes = [
+        {"id": 1, "name": "Massive Muesli"},
+        {"id": 2, "name": "Cauliflower, Chickpea and Kale Curry"},
+        {"id": 3, "name": "Bean Chilli"}
+    ]
+    return {"recipes": recipes}
+
 async def root():
-    return {"message": "Hello World"}
+    return get_recipes()
+
